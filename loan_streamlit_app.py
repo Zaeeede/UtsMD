75c04d5f7631f05c6939c74899295eaa991db792
Dennis Purnomo Yohaidi <160407221+Zaeeede@users.noreply.github.com>
2025-04-20 20:25:29 +0700

    Update loan_streamlit_app.py

--- loan_streamlit_app.py
+++ loan_streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle as pkl
-from model_xgb import ModelXGB  # pastikan class ModelXGB disimpan di file ini
+import pickle as pkl  
 
 # Load objek-objek model dan preprocessing
 with open('model_xgb.pkl', 'rb') as file:
